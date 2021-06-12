062329ca4183685920fafd3b89d673b69b36c3a1
Guillaume Docquier <guillaumedocquier@hotmail.com>
2021-06-12 01:50:01 -0400

    Improved the layout

--- src/crowcraft/src/components/layout/footer/Footer.jsx
+++ src/crowcraft/src/components/layout/footer/Footer.jsx
@@ -1,6 +1,7 @@
 export const Footer = ({ className }) => (
-    <div className={className + " flex flex-column items-center justify-center f5 bg-bittersweet light-yellow"}>
-        <div>Crowcraft - Made by Guike for Valeria</div>
-        <div>With the contribution of Bluezinhaaa, Shilana, Upem and BeardslayTTV</div>
+    <div className={className + " flex flex-column items-center justify-center f7 bg-marine"}>
+        <div>Crowcraft is sponsored by Valeria Gaming</div>
+        <div>Made with ❤ by Guike</div>
+        <div>Special thanks to Upem, Bluezinhaaa and Shilana</div>
     </div>
 );
--- src/crowcraft/src/components/layout/header/Header.jsx
+++ src/crowcraft/src/components/layout/header/Header.jsx
@@ -1,8 +1,7 @@
-import logo from "./logo.png";
+import logo from "./logo.svg";
 
 export const Header = ({ className }) => (
-    <div className={className + " flex items-center bg-mauve-taupe"}>
-        <img src={logo} alt="Crowcraft logo" className="h-100" />
-        <div className="pl3" >Crowcraft</div>
+    <div className={className + " flex items-center bg-marine bb bw1"}>
+        <img src={logo} alt="Crowcraft logo" className="h-50" />
     </div>
 );
