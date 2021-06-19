2fa585478aa04ccb3db567db4f34e3ceee8dc2f9
Guillaume Docquier <guillaumedocquier@hotmail.com>
2021-06-19 02:12:28 -0400

    Metal Block

---
+++ src/crowcraft/src/data/items/metal-block.js
@@ -0,0 +1,21 @@
+import { Item, CraftingMaterial, Rarities, Professions } from "models";
+import { Ore } from "./ores";
+
+/*
+    TODO This item has +[2, 4, 6, 8, ?10?] Attack Power, which is not the same scale as other attack power stats
+*/
+export class MetalBlock extends Item {
+    constructor() {
+        super(
+            "metal block",
+            [Professions.Armorsmith, Professions.Weaponsmith],
+            [Rarities.Common, Rarities.Uncommon, Rarities.Rare, Rarities.Epic, Rarities.Legendary],
+            [
+                new CraftingMaterial(3, new Ore()),
+                new CraftingMaterial(3, new Ore()),
+                new CraftingMaterial(3, new Ore()),
+            ],
+            1
+        );
+    }
+}
