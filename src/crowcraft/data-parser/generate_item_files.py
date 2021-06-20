badb783ae3b55d9f8a5479de0d8cbd9295a368ec
Guillaume Docquier <guillaumedocquier@hotmail.com>
2021-06-20 01:13:44 -0400

    Added try catch to highligh problems in data format

--- src/crowcraft/data-parser/generate_item_files.py
+++ src/crowcraft/data-parser/generate_item_files.py
@@ -76,8 +76,11 @@ def extract_item_data(item):
     class_name = make_class_name(item_name)
     professions = item[Columns.REQUIRED_PROFESSIONS].split(" - ")
 
-    item_rarities = item[Columns.RARITIES].lower().split("-")
-    item_rarities = rarity_names[rarity_ranks[item_rarities[0]]:rarity_ranks[item_rarities[min(1, len(item_rarities) - 1)]] + 1]
+    item_rarities = item[Columns.RARITIES].lower().replace(" ", "").split("-")
+    try:
+        item_rarities = rarity_names[rarity_ranks[item_rarities[0]]:rarity_ranks[item_rarities[min(1, len(item_rarities) - 1)]] + 1]
+    except:
+        print(bcolors.FAIL, f"Invalid item rarities {{{item_rarities}}} for item: {{{item_name}}}", bcolors.ENDC)
 
     raw_crafting_materials = item[Columns.MATERIALS].lower().replace("non-basic ", "").split(", ")
     crafting_materials = []
@@ -125,8 +128,17 @@ export class {class_name} extends Item {
     (file_name, class_name, item_name, professions, item_rarities, crafting_materials, quantity_per_craft, _) = item_data
 
     crafting_materials_set = set([crafting_material_name for (_, crafting_material_name) in crafting_materials])
-    imports = [f"import {{ {make_class_name(crafting_material_name)} }} from \"./{make_file_name_without_extension(crafting_material_name)}\";" for crafting_material_name in crafting_materials_set]
-    js_crafting_materials = [f"new CraftingMaterial({quantity}, new {make_class_name(crafting_material_name)}())," for (quantity, crafting_material_name) in crafting_materials]
+    try:
+        imports = [f"import {{ {make_class_name(crafting_material_name)} }} from \"./{make_file_name_without_extension(crafting_material_name)}\";" for crafting_material_name in crafting_materials_set]
+    except:
+        imports = []
+        print(bcolors.FAIL, f"Cannot create imports properly from item set {{{crafting_materials_set}}} for item: {{{item_name}}}", bcolors.ENDC)
+
+    try:
+        js_crafting_materials = [f"new CraftingMaterial({quantity}, new {make_class_name(crafting_material_name)}())," for (quantity, crafting_material_name) in crafting_materials]
+    except:
+        js_crafting_materials = []
+        print(bcolors.FAIL, f"Cannot create crafting materials properly from crafting_materials {{{crafting_materials}}} for item: {{{item_name}}}", bcolors.ENDC)
 
     return js_code.replace("{imports}", "\n".join(imports))\
         .replace("{class_name}", class_name)\
