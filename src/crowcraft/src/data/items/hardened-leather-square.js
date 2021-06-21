404c4d465ec712dc8c6a567ebcfe33ba59893e5f
Guillaume Docquier <guillaumedocquier@hotmail.com>
2021-06-21 18:25:21 -0400

    Hardened leather square

--- src/crowcraft/src/data/items/hardened-leather-square.js
+++ src/crowcraft/src/data/items/hardened-leather-square.js
@@ -1,5 +1,10 @@
 import { CustomizableComponent, Customization, CraftingMaterial, Rarities, Professions, ItemsStats } from "models";
 import { Hide } from "./hide";
+import { DurableHide } from "./durable-hide";
+import { FlexibleHide } from "./flexible-hide";
+import { SoftHide } from "./soft-hide";
+import { ToughHide } from "./tough-hide";
+import { StrongHide } from "./strong-hide";
 import { LacingSinew } from "./lacing-sinew";
 
 export class HardenedLeatherSquare extends CustomizableComponent {
@@ -12,13 +17,276 @@ export class HardenedLeatherSquare extends CustomizableComponent {
                 new CraftingMaterial(5, new Hide()),
 				new CraftingMaterial(5, new Hide()),
 				new CraftingMaterial(5, new Hide()),
-				new CraftingMaterial(3, new LacingSinew()),
+				new CraftingMaterial(5, new LacingSinew()),
             ],
             1,
             [
-                // TODO Customizations
+                new LimberCoriumHardenedLeatherSquareCustomization(),
+                new SoftCoriumHardenedLeatherSquareCustomization(),
+                new SturdyCoriumHardenedLeatherSquareCustomization(),
+                new StoutCoriumHardenedLeatherSquareCustomization(),
+                new CoriumHardenedLeatherSquareCustomization(),
+                new ChamoiseHardenedLeatherSquareCustomization(),
+                new StrudyBicastHardenedLeatherSquareCustomization(),
+                new LimberGrainHardenedLeatherSquareCustomization(),
+                new SplitGrainHardenedLeatherSquareCustomization(),
+                new NappaHardenedLeatherSquareCustomization(),
+                new BicastHardenedLeatherSquareCustomization(),
+                new SuedeHardenedLeatherSquareCustomization(),
             ]
         );
+    }
+}
+
+class LimberCoriumHardenedLeatherSquareCustomization extends Customization {
+    constructor() {
+        super(
+            "limber corium",
+            [
+                new CraftingMaterial(5, new DurableHide()),
+				new CraftingMaterial(5, new FlexibleHide()),
+				new CraftingMaterial(5, new FlexibleHide()),
+				new CraftingMaterial(5, new LacingSinew()),
+            ],
+            {
+                [Rarities.Common.name]: [ItemsStats.BleedDamageBonus],
+                [Rarities.Uncommon.name]: [ItemsStats.BleedDamageBonus],
+                [Rarities.Rare.name]: [ItemsStats.BleedDamageBonus],
+                [Rarities.Epic.name]: [ItemsStats.BleedDamageBonus],
+                [Rarities.Legendary.name]: [ItemsStats.BleedDamageBonus]
+            }
+        )
+    }
+}
+
+class SoftCoriumHardenedLeatherSquareCustomization extends Customization {
+    constructor() {
+        super(
+            "soft corium",
+            [
+                new CraftingMaterial(5, new DurableHide()),
+				new CraftingMaterial(5, new SoftHide()),
+				new CraftingMaterial(5, new SoftHide()),
+				new CraftingMaterial(5, new LacingSinew()),
+            ],
+            {
+                [Rarities.Common.name]: [ItemsStats.FireDamageBonus],
+                [Rarities.Uncommon.name]: [ItemsStats.FireDamageBonus],
+                [Rarities.Rare.name]: [ItemsStats.FireDamageBonus],
+                [Rarities.Epic.name]: [ItemsStats.FireDamageBonus],
+                [Rarities.Legendary.name]: [ItemsStats.FireDamageBonus]
+            }
+        )
+    }
+}
+
+class SturdyCoriumHardenedLeatherSquareCustomization extends Customization {
+    constructor() {
+        super(
+            "sturdy corium",
+            [
+                new CraftingMaterial(5, new DurableHide()),
+				new CraftingMaterial(5, new StrongHide()),
+				new CraftingMaterial(5, new StrongHide()),
+				new CraftingMaterial(5, new LacingSinew()),
+            ],
+            {
+                [Rarities.Common.name]: [ItemsStats.IceDamageBonus],
+                [Rarities.Uncommon.name]: [ItemsStats.IceDamageBonus],
+                [Rarities.Rare.name]: [ItemsStats.IceDamageBonus],
+                [Rarities.Epic.name]: [ItemsStats.IceDamageBonus],
+                [Rarities.Legendary.name]: [ItemsStats.IceDamageBonus]
+            }
+        )
+    }
+}
+
+class StoutCoriumHardenedLeatherSquareCustomization extends Customization {
+    constructor() {
+        super(
+            "stout corium",
+            [
+                new CraftingMaterial(5, new DurableHide()),
+				new CraftingMaterial(5, new ToughHide()),
+				new CraftingMaterial(5, new ToughHide()),
+				new CraftingMaterial(5, new LacingSinew()),
+            ],
+            {
+                [Rarities.Common.name]: [ItemsStats.ElectricityDamageBonus],
+                [Rarities.Uncommon.name]: [ItemsStats.ElectricityDamageBonus],
+                [Rarities.Rare.name]: [ItemsStats.ElectricityDamageBonus],
+                [Rarities.Epic.name]: [ItemsStats.ElectricityDamageBonus],
+                [Rarities.Legendary.name]: [ItemsStats.ElectricityDamageBonus]
+            }
+        )
+    }
+}
+
+class CoriumHardenedLeatherSquareCustomization extends Customization {
+    constructor() {
+        super(
+            "corium",
+            [
+                new CraftingMaterial(5, new FlexibleHide()),
+				new CraftingMaterial(5, new DurableHide()),
+				new CraftingMaterial(5, new DurableHide()),
+				new CraftingMaterial(5, new LacingSinew()),
+            ],
+            {
+                [Rarities.Common.name]: [ItemsStats.NatureDamageBonus],
+                [Rarities.Uncommon.name]: [ItemsStats.NatureDamageBonus],
+                [Rarities.Rare.name]: [ItemsStats.NatureDamageBonus],
+                [Rarities.Epic.name]: [ItemsStats.NatureDamageBonus],
+                [Rarities.Legendary.name]: [ItemsStats.NatureDamageBonus]
+            }
+        )
+    }
+}
+
+class ChamoiseHardenedLeatherSquareCustomization extends Customization {
+    constructor() {
+        super(
+            "chamoise",
+            [
+                new CraftingMaterial(5, new FlexibleHide()),
+				new CraftingMaterial(5, new SoftHide()),
+				new CraftingMaterial(5, new SoftHide()),
+				new CraftingMaterial(5, new LacingSinew()),
+            ],
+            {
+                [Rarities.Common.name]: [ItemsStats.PoisonDamageBonus],
+                [Rarities.Uncommon.name]: [ItemsStats.PoisonDamageBonus],
+                [Rarities.Rare.name]: [ItemsStats.PoisonDamageBonus],
+                [Rarities.Epic.name]: [ItemsStats.PoisonDamageBonus],
+                [Rarities.Legendary.name]: [ItemsStats.PoisonDamageBonus]
+            }
+        )
+    }
+}
+
+class StrudyBicastHardenedLeatherSquareCustomization extends Customization {
+    constructor() {
+        super(
+            "sturdy bicast",
+            [
+                new CraftingMaterial(5, new FlexibleHide()),
+				new CraftingMaterial(5, new StrongHide()),
+				new CraftingMaterial(5, new StrongHide()),
+				new CraftingMaterial(5, new LacingSinew()),
+            ],
+            {
+                [Rarities.Common.name]: [ItemsStats.DiseaseDamageBonus],
+                [Rarities.Uncommon.name]: [ItemsStats.DiseaseDamageBonus],
+                [Rarities.Rare.name]: [ItemsStats.DiseaseDamageBonus],
+                [Rarities.Epic.name]: [ItemsStats.DiseaseDamageBonus],
+                [Rarities.Legendary.name]: [ItemsStats.DiseaseDamageBonus]
+            }
+        )
+    }
+}
+
+class LimberGrainHardenedLeatherSquareCustomization extends Customization {
+    constructor() {
+        super(
+            "limber grain",
+            [
+                new CraftingMaterial(5, new FlexibleHide()),
+				new CraftingMaterial(5, new ToughHide()),
+				new CraftingMaterial(5, new ToughHide()),
+				new CraftingMaterial(5, new LacingSinew()),
+            ],
+            {
+                [Rarities.Common.name]: [ItemsStats.SlashingDamageBonus],
+                [Rarities.Uncommon.name]: [ItemsStats.SlashingDamageBonus],
+                [Rarities.Rare.name]: [ItemsStats.SlashingDamageBonus],
+                [Rarities.Epic.name]: [ItemsStats.SlashingDamageBonus],
+                [Rarities.Legendary.name]: [ItemsStats.SlashingDamageBonus]
+            }
+        )
+    }
+}
+
+class SplitGrainHardenedLeatherSquareCustomization extends Customization {
+    constructor() {
+        super(
+            "split grain",
+            [
+                new CraftingMaterial(5, new SoftHide()),
+				new CraftingMaterial(5, new DurableHide()),
+				new CraftingMaterial(5, new DurableHide()),
+				new CraftingMaterial(5, new LacingSinew()),
+            ],
+            {
+                [Rarities.Common.name]: [ItemsStats.CrushingDamageBonus],
+                [Rarities.Uncommon.name]: [ItemsStats.CrushingDamageBonus],
+                [Rarities.Rare.name]: [ItemsStats.CrushingDamageBonus],
+                [Rarities.Epic.name]: [ItemsStats.CrushingDamageBonus],
+                [Rarities.Legendary.name]: [ItemsStats.CrushingDamageBonus]
+            }
+        )
+    }
+}
+
+class NappaHardenedLeatherSquareCustomization extends Customization {
+    constructor() {
+        super(
+            "nappa",
+            [
+                new CraftingMaterial(5, new SoftHide()),
+				new CraftingMaterial(5, new FlexibleHide()),
+				new CraftingMaterial(5, new FlexibleHide()),
+				new CraftingMaterial(5, new LacingSinew()),
+            ],
+            {
+                [Rarities.Common.name]: [ItemsStats.PiercingDamageBonus],
+                [Rarities.Uncommon.name]: [ItemsStats.PiercingDamageBonus],
+                [Rarities.Rare.name]: [ItemsStats.PiercingDamageBonus],
+                [Rarities.Epic.name]: [ItemsStats.PiercingDamageBonus],
+                [Rarities.Legendary.name]: [ItemsStats.PiercingDamageBonus]
+            }
+        )
+    }
+}
+
+class BicastHardenedLeatherSquareCustomization extends Customization {
+    constructor() {
+        super(
+            "bicast",
+            [
+                new CraftingMaterial(5, new StrongHide()),
+				new CraftingMaterial(5, new FlexibleHide()),
+				new CraftingMaterial(5, new FlexibleHide()),
+				new CraftingMaterial(5, new LacingSinew()),
+            ],
+            {
+                [Rarities.Common.name]: [ItemsStats.CriticalDamage],
+                [Rarities.Uncommon.name]: [ItemsStats.CriticalDamage],
+                [Rarities.Rare.name]: [ItemsStats.CriticalDamage],
+                [Rarities.Epic.name]: [ItemsStats.CriticalDamage],
+                [Rarities.Legendary.name]: [ItemsStats.CriticalDamage]
+            }
+        )
+    }
+}
+
+class SuedeHardenedLeatherSquareCustomization extends Customization {
+    constructor() {
+        super(
+            "suede",
+            [
+                new CraftingMaterial(5, new StrongHide()),
+                new CraftingMaterial(5, new SoftHide()),
+                new CraftingMaterial(5, new SoftHide()),
+				new CraftingMaterial(5, new LacingSinew()),
+            ],
+            {
+                [Rarities.Common.name]: [ItemsStats.CriticalHealingAmount],
+                [Rarities.Uncommon.name]: [ItemsStats.CriticalHealingAmount],
+                [Rarities.Rare.name]: [ItemsStats.CriticalHealingAmount],
+                [Rarities.Epic.name]: [ItemsStats.CriticalHealingAmount],
+                [Rarities.Legendary.name]: [ItemsStats.CriticalHealingAmount]
+            }
+        )
     }
 }
     
