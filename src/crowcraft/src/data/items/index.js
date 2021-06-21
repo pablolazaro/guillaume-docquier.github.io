c9b09ef9f77b3e9b5931b01dffa889186199f046
Guillaume Docquier <guillaumedocquier@hotmail.com>
2021-06-20 23:50:25 -0400

    All hides

--- src/crowcraft/src/data/items/index.js
+++
@@ -1,6 +0,0 @@
-export * from "./ore";
-export * from "./aurelium";
-export * from "./copper";
-export * from "./iron";
-export * from "./silver";
-export * from "./tin";
---
+++ src/crowcraft/src/data/items/tough-hide.js
@@ -0,0 +1,14 @@
+import { Rarities } from "models";
+import { Hide } from "./hide";
+
+export class ToughHide extends Hide {
+    constructor() {
+        super(
+            "tough hide",
+            [],
+            [Rarities.Common, Rarities.Uncommon, Rarities.Rare, Rarities.Epic, Rarities.Legendary],
+            [],
+            1
+        );
+    }
+}
