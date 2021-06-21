40f6346fbfcbb70a01b33b0ed31325885cd618de
Guillaume Docquier <guillaumedocquier@hotmail.com>
2021-06-20 20:52:28 -0400

    Book chapter

--- src/crowcraft/src/data/items/book-chapter.js
+++ src/crowcraft/src/data/items/book-chapter.js
@@ -2,6 +2,11 @@ import { CustomizableComponent, Customization, CraftingMaterial, Rarities, Profe
 import { StitchedLeather } from "./stitched-leather";
 import { CompositeWoodPlank } from "./composite-wood-plank";
 import { Stone } from "./stone";
+import { Travertine } from "./travertine";
+import { Marble } from "./marble";
+import { Limestone } from "./limestone";
+import { Granite } from "./granite";
+import { Slate } from "./slate";
 
 export class BookChapter extends CustomizableComponent {
     constructor() {
@@ -16,9 +21,113 @@ export class BookChapter extends CustomizableComponent {
             ],
             1,
             [
-                // TODO Customizations
+                new RedemptionBookChapterCustomization(),
+                new SinBookChapterCustomization(),
+                new IndulgenceBookChapterCustomization(),
+                new FervorBookChapterCustomization(),
+                new AbsolutionBookChapterCustomization(),
             ]
         );
+    }
+}
+
+class RedemptionBookChapterCustomization extends Customization {
+    constructor() {
+        super(
+            "redemption",
+            [
+                new CraftingMaterial(1, new CompositeWoodPlank()),
+				new CraftingMaterial(1, new StitchedLeather()),
+				new CraftingMaterial(3, new Travertine()),
+            ],
+            {
+                [Rarities.Common.name]: [ItemsStats.SlashingResistance],
+                [Rarities.Uncommon.name]: [ItemsStats.SlashingResistance],
+                [Rarities.Rare.name]: [ItemsStats.SlashingResistance],
+                [Rarities.Epic.name]: [ItemsStats.SlashingResistance],
+                [Rarities.Legendary.name]: [ItemsStats.SlashingResistance]
+            }
+        )
+    }
+}
+
+class SinBookChapterCustomization extends Customization {
+    constructor() {
+        super(
+            "sin",
+            [
+                new CraftingMaterial(1, new CompositeWoodPlank()),
+				new CraftingMaterial(1, new StitchedLeather()),
+				new CraftingMaterial(3, new Marble()),
+            ],
+            {
+                [Rarities.Common.name]: [ItemsStats.FireResistance],
+                [Rarities.Uncommon.name]: [ItemsStats.FireResistance],
+                [Rarities.Rare.name]: [ItemsStats.FireResistance],
+                [Rarities.Epic.name]: [ItemsStats.FireResistance],
+                [Rarities.Legendary.name]: [ItemsStats.FireResistance]
+            }
+        )
+    }
+}
+
+class IndulgenceBookChapterCustomization extends Customization {
+    constructor() {
+        super(
+            "indulgence",
+            [
+                new CraftingMaterial(1, new CompositeWoodPlank()),
+				new CraftingMaterial(1, new StitchedLeather()),
+				new CraftingMaterial(3, new Limestone()),
+            ],
+            {
+                [Rarities.Common.name]: [ItemsStats.CrushingResistance],
+                [Rarities.Uncommon.name]: [ItemsStats.CrushingResistance],
+                [Rarities.Rare.name]: [ItemsStats.CrushingResistance],
+                [Rarities.Epic.name]: [ItemsStats.CrushingResistance],
+                [Rarities.Legendary.name]: [ItemsStats.CrushingResistance]
+            }
+        )
+    }
+}
+
+class FervorBookChapterCustomization extends Customization {
+    constructor() {
+        super(
+            "fervor",
+            [
+                new CraftingMaterial(1, new CompositeWoodPlank()),
+				new CraftingMaterial(1, new StitchedLeather()),
+				new CraftingMaterial(3, new Granite()),
+            ],
+            {
+                [Rarities.Common.name]: [ItemsStats.ElectricityResistance],
+                [Rarities.Uncommon.name]: [ItemsStats.ElectricityResistance],
+                [Rarities.Rare.name]: [ItemsStats.ElectricityResistance],
+                [Rarities.Epic.name]: [ItemsStats.ElectricityResistance],
+                [Rarities.Legendary.name]: [ItemsStats.ElectricityResistance]
+            }
+        )
+    }
+}
+
+class AbsolutionBookChapterCustomization extends Customization {
+    constructor() {
+        super(
+            "absolution",
+            [
+                new CraftingMaterial(1, new CompositeWoodPlank()),
+				new CraftingMaterial(1, new StitchedLeather()),
+				new CraftingMaterial(3, new Slate()),
+            ],
+            {
+                [Rarities.Common.name]: [ItemsStats.PiercingResistance],
+                [Rarities.Uncommon.name]: [ItemsStats.PiercingResistance],
+                [Rarities.Rare.name]: [ItemsStats.PiercingResistance],
+                [Rarities.Epic.name]: [ItemsStats.PiercingResistance],
+                [Rarities.Legendary.name]: [ItemsStats.PiercingResistance]
+            }
+        )
     }
 }
     
