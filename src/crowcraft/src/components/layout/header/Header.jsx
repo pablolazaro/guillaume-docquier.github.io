137e1689b03d738d98bff8f9aafd41155cdfc57a
Guillaume Docquier <guillaumedocquier@hotmail.com>
2021-07-04 22:35:02 -0400

    Promote to beta

--- src/crowcraft/src/components/layout/header/Header.jsx
+++ src/crowcraft/src/components/layout/header/Header.jsx
@@ -7,7 +7,7 @@ export const Header = ({ className }) => (
     <div className={className + " flex justify-between items-center bg-marine bb bw1"}>
         <div className="flex items-center h-100">
             <img src={logo} alt="Crowcraft logo" className="h-50 mr2" />
-            <div className="tag fw5 f7 pt2">ALPHA 6.540</div>
+            <div className="tag fw5 f7 pt2">BETA 6.540</div>
         </div>
         <div className="flex items-center">
             <a className="mr4 w7 tc" href="https://discord.gg/Ju87JBQGJw" title="Join our Discord!" target="_blank" rel="noopener noreferrer">Found a bug or want to help?</a>
