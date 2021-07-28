87bdacd930f926caab73d6c856b7a28a185fe74c
Guillaume Docquier <guillaumedocquier@hotmail.com>
2021-07-27 20:26:14 -0400

    Make the layout a bit larger

--- src/crowcraft/src/components/layout/Layout.jsx
+++ src/crowcraft/src/components/layout/Layout.jsx
@@ -5,7 +5,7 @@ import { Footer } from "./footer";
 export const Layout = ({ children }) => (
     <div className="bg-charcoal peach-puff">
         <Header className="nmb5" />
-        <Content className="flex pv5 min-vh-100 ph2 m0auto mw8">
+        <Content className="flex pv5 min-vh-100 ph2 m0auto mw9">
             {children}
         </Content>
         <Footer className="h5 nmt5" />
--- src/crowcraft/src/components/layout/header/Header.jsx
+++ src/crowcraft/src/components/layout/header/Header.jsx
@@ -5,7 +5,7 @@ import discord from "./discord.svg";
 
 export const Header = ({ className }) => (
     <div className={className + " flex bg-marine bb bw1"}>
-        <div className="flex justify-between items-center flex-wrap w-100 ph2 m0auto mw8">
+        <div className="flex justify-between items-center flex-wrap w-100 ph2 m0auto mw9">
             <div className="flex items-center pv3">
                 <img src={logo} alt="Crowcraft logo" className="h2 mr2" />
                 <div className="tag fw5 f7 pt2">BETA 6.540</div>
