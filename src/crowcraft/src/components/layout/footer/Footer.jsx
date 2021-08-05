856e35b41e94cb4f81b0344e6cc09a8420096182
Guillaume Docquier <guillaumedocquier@hotmail.com>
2021-08-04 20:56:10 -0400

    Deploy a tweak to paypal button title

--- src/crowcraft/src/components/layout/footer/Footer.jsx
+++ src/crowcraft/src/components/layout/footer/Footer.jsx
@@ -13,7 +13,7 @@ export const Footer = ({ className }) => (
                     <input type="hidden" name="no_recurring" value="0" />
                     <input type="hidden" name="item_name" value="This will support Guike, the creator of Crowcraft" />
                     <input type="hidden" name="currency_code" value="CAD" />
-                    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="PayPal - The safer, easier way to pay online!" alt="Donate with PayPal button" />
+                    <input type="image" src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" name="submit" title="Donate via PayPal" alt="Donate with PayPal button" />
                 </form>
             </div>
         </div>
