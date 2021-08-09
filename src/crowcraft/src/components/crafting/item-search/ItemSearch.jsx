462aef0d382286f12ff342b9f56d7929008c3fdb
Pablo Lázaro <pablolp90@gmail.com>
2021-08-09 12:22:25 +0200

    Improve search performance

--- src/crowcraft/src/components/crafting/item-search/ItemSearch.jsx
+++ src/crowcraft/src/components/crafting/item-search/ItemSearch.jsx
@@ -3,39 +3,35 @@ import "./ItemSearch.css";
 import { items } from "data";
 import { TextInput, Item } from "components";
 import { Sorting } from "utils";
-import { memo, useState, useEffect, useCallback } from "react";
+import { memo, useState, useEffect, useMemo, useCallback } from "react";
 
 const MAX_ITEMS_COUNT = 30;
 
-export const ItemSearch = memo(({ onItemSelected }) => {
-    const [allItems] = useState(
-        items
-            .map(item => new item())
-            .filter(item => item.isCraftable)
-            .sort(Sorting.ascending(item => item.name))
-            .sort(Sorting.descending(item => item.craftingRank))
-    );
+const allItems =
+    items
+        .map(item => new item())
+        .filter(item => item.isCraftable)
+        .sort(Sorting.ascending(item => item.name))
+        .sort(Sorting.descending(item => item.craftingRank));
 
+const filterItems = text => allItems.filter(item => item.name.includes(text));
+
+export const ItemSearch = memo(({ onItemSelected }) => {
     const [filter, setFilter] = useState("");
     const [matchedItems, setMatchedItems] = useState(allItems);
     const [selectedItemId, setSelectedItemId] = useState(null);
 
-    useEffect(() => {
-        const timeout = setTimeout(() => {
-            setMatchedItems(allItems.filter(item => {
-                if (filter.length === 0) {
-                    return true;
-                }
+    const filteredItems = useMemo(() => (filter.length === 0 ? allItems : filterItems(filter)), [filter]);
 
-                return item.name.includes(filter);
-            }))
-        }, 300);
+    useEffect(() => setMatchedItems(filteredItems), [filteredItems]);
 
-        return () => clearTimeout(timeout);
-    }, [filter, allItems]);
+    let searchDebounce = null;
 
     const onFilterChanged = e => {
-        setFilter(e.target.value.toLowerCase());
+        clearTimeout(searchDebounce);
+        searchDebounce = setTimeout(() => {
+            setFilter(e.target.value.toLowerCase());
+        }, 300);
     };
 
     const selectItem = useCallback(item => {
